647c4686bb48e2a48b6f5c5fec70eb37e83a1e63
Piergiorgio Piras <piraspiergiorgio5@gmail.com>
2022-03-11 15:15:56 +0100

    -start-

---
+++ js/script.js
@@ -0,0 +1,33 @@
+const app = new Vue({
+    el : '#root',
+    data : {
+        arrSlides : [
+            {
+                title : 'Svezia',
+                img : '01.jpg',
+                text : 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+            },
+            {
+                title : 'Svizzera',
+                img : '02.jpg',
+                text : 'Lorem ipsum.',
+            },
+            {
+                title : 'Gran Bretagna',
+                img : '03.jpg',
+                text : 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+            },
+            {
+                title : 'Germania',
+                img : '04.jpg',
+                text : 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+            },
+            {
+                title : 'Paradise',
+                img : '05.jpg',
+                text : 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+            },
+        ],
+        
+    }
+})
